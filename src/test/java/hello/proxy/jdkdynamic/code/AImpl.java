dbd0c09a179f839c81d2f21cf51c2b0197ff481f
Cho Chan Hee <cksgml4424@gmail.com>
2021-11-18 08:50:08 +0900

    동적 프록시 커밋

--- src/test/java/hello/proxy/jdkdynamic/code/AImpl.java
+++ src/test/java/hello/proxy/jdkdynamic/code/AImpl.java
@@ -3,7 +3,7 @@ package hello.proxy.jdkdynamic.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Almpl implements AInterface {
+public class AImpl implements AInterface {
 	@Override
 	public String call() {
 		log.info("A 호출");
